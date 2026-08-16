f5bbbda14288935f6d9a58674b272a48902586ab
Shouvik Majumder <138254053+shouvikmajumder@users.noreply.github.com>
2026-08-15 21:21:17 -0400

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        '''
+            Problem Approach: 
+                We are going to use a sliding window
+                
+                In order to make profit the value of our right pointer must be bigger than 
+                our left pointer value
+
+        '''
+        max_prof = 0
+
+        left = 0
+
+        for right in range(len(prices)):
+            if prices[right] >= prices[left]: 
+                curr_prof = prices[right] - prices[left]
+                max_prof = max(max_prof,curr_prof)
+            elif prices[right] < prices[left]:
+                while left < right: 
+                    max_prof = max(max_prof,prices[right] - prices[left])
+                    left += 1
+        
+        return max_prof
