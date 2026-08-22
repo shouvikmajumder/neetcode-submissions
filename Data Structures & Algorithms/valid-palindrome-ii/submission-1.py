b9827c6ec200ddc5dc8b1ba718d84a7072b0eb34
Shouvik Majumder <138254053+shouvikmajumder@users.noreply.github.com>
2026-08-22 13:29:46 -0400

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        missahapps = 0 
+
+        left,right = 0,len(s) -1
+
+        while left < right:
+            
+            if s[left] != s[right]: 
+                if missahapps == 1: 
+                    return False
+                missahapps += 1
+                left += 1 
+                right -= 1
+            else: 
+                left += 1
+                right -= 1         
+
+        return True
