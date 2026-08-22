99dcf4c8cb20b96e23dc64935d0c6ef55e50b846
Shouvik Majumder <138254053+shouvikmajumder@users.noreply.github.com>
2026-08-22 13:16:39 -0400

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        # brute force, O(n^2) sol where we are just oging to interate over the nums untill we get to k
+
+        res = 0
+
+        for i in range(len(nums)): 
+            for j in range(i,len(nums)):
+                subarrsum = sum(nums[i: j +1])
+                
+                if subarrsum == k:
+                    res += 1
+        return res 
